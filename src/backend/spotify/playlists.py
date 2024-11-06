27da1b916d4509af0ac4c933883280ba8f3614d5
cbisaccia78 <FEFQ9F1qaz0>
2024-11-06 14:35:57 -0500

    audio analysis/features

--- src/backend/spotify/playlists.py
+++ src/backend/spotify/playlists.py
@@ -8,7 +8,7 @@ class PlaylistManager:
         if playlist_id is not None:
             self.load_playlist(playlist_id)
 
-    def load_playlist(self,playlist_id, fields="name,images,tracks(items(track(name,album(name,images),artists(name)))"):
+    def load_playlist(self,playlist_id, fields="name,images,tracks(items(track(name,id,album(name,images),artists(name)))"):
         # Define the base URL for the playlist info request
         url = f"https://api.spotify.com/v1/playlists/{playlist_id}"
         
@@ -43,10 +43,55 @@ class PlaylistManager:
 class Track:
     def __init__(self, track_dict):
         self.name = track_dict['name']
+        self.id = track_dict['id']
         self.artists = [artist['name'] for artist in track_dict['artists']]
         self.album_name = track_dict['album']['name']
         self.album_images = track_dict['album']['images']
+        self.audio_features = None
+        self.audio_analysis = None
         self.rating = None
+    
+    def load_audio_features(self):
+        # Define the base URL for the playlist info request
+        url = f"https://api.spotify.com/v1/audio-features/{self.id}"
+        
+        # Set the headers, including the Authorization Bearer token
+        headers = {
+            "Authorization": f"Bearer {os.environ['access_token']}"
+        }
+        
+        # Make the GET request to retrieve playlist information
+        response = requests.get(url, headers=headers)
+        
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Parse the JSON response and return playlist info
+            print(response.json().keys())
+        else:
+            # Print the error response for debugging
+            print(f"Error: {response.status_code}, {response.text}")
+            raise RuntimeError("")
+
+    def load_audio_analysis(self):
+        # Define the base URL for the playlist info request
+        url = f"https://api.spotify.com/v1/audio-analysis/{self.id}"
+        
+        # Set the headers, including the Authorization Bearer token
+        headers = {
+            "Authorization": f"Bearer {os.environ['access_token']}"
+        }
+        
+        # Make the GET request to retrieve playlist information
+        response = requests.get(url, headers=headers)
+        
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Parse the JSON response and return playlist info
+            print(response.json().keys())
+        else:
+            # Print the error response for debugging
+            print(f"Error: {response.status_code}, {response.text}")
+            raise RuntimeError("")
 
     
 if __name__ == '__main__':
@@ -63,5 +108,9 @@ if __name__ == '__main__':
     pm = PlaylistManager()
     pm.load_playlist(playlist_id)
     print(list(pm.playlist.keys()))
-    print(pm.get_tracks()[0].name)
-    print(pm.name)
+    track = pm.get_tracks()[0]
+    print(track.name)
+    print(track.id)
+    track.load_audio_analysis()
+    track.load_audio_features()
+    
