fab1ba8eccb6b855645af145c65ab05c7e41f170
cbisaccia78 <FEFQ9F1qaz0>
2024-11-07 14:28:09 -0500

    bug in layer shape

--- src/backend/models/rating_predictor.py
+++ src/backend/models/rating_predictor.py
@@ -5,7 +5,6 @@ import numpy as np
 from keras.layers import Dense
 from ml_utils.transformations import string_to_1hot_sequence, split_vector, flatten_dict_values, normalize
 from ml_utils.validation import strat_kfold
-import pdb
 
 def create_rating_model(tracks):
     ## Create dataset and labels
@@ -117,39 +116,47 @@ def create_rating_model(tracks):
     track_audio_analysis_train, track_audio_analysis_valid, track_audio_analysis_test = split_vector(track_audio_analysis, 0.6, 0.8)
 
     track_ratings_train, track_ratings_valid, track_ratings_test = split_vector(track_ratings, 0.6, 0.8)
-    pdb.set_trace()
+
     ## normalize features/analysis
     feature_mean = track_audio_features_train.mean(axis=0)
     feature_std = track_audio_features_train.std(axis=0)
+    
+    # drop any feature columns with 0 standard deviation
+    nonzero_std_idx = np.where(feature_std != 0)[0]
+
+    track_audio_features_train = track_audio_features_train[:, nonzero_std_idx]
+    track_audio_features_valid = track_audio_features_valid[:, nonzero_std_idx]
+    track_audio_features_test = track_audio_features_test[:, nonzero_std_idx]
+    feature_mean = feature_mean[nonzero_std_idx]
+    feature_std = feature_std[nonzero_std_idx]
+
     normalize(track_audio_features_train, feature_mean, feature_std)
     normalize(track_audio_features_valid, feature_mean, feature_std)
     normalize(track_audio_features_test, feature_mean, feature_std)
 
-    ## drop any feature columns with 0 standard deviation
-    nonzero_std_idx = np.where(feature_std != 0)[0]
-    track_audio_features_train = track_audio_features_train[:, nonzero_std_idx]
-    track_audio_features_valid = track_audio_features_valid[:, nonzero_std_idx]
-    track_audio_features_test = track_audio_features_test[:, nonzero_std_idx]
-
     analysis_mean = track_audio_analysis_train.mean(axis=0)
     analysis_std = track_audio_analysis_train.std(axis=0)
-    normalize(track_audio_analysis_train, analysis_mean, analysis_std)
-    normalize(track_audio_analysis_valid, analysis_mean, analysis_std)
-    normalize(track_audio_analysis_test, analysis_mean, analysis_std)
 
-    ## drop any analysis columns with 0 standard deviation
+    # drop any analysis columns with 0 standard deviation
     nonzero_std_idx = np.where(analysis_std != 0)[0]
     track_audio_analysis_train = track_audio_analysis_train[:, nonzero_std_idx]
     track_audio_analysis_valid = track_audio_analysis_valid[:, nonzero_std_idx]
     track_audio_analysis_test = track_audio_analysis_test[:, nonzero_std_idx]
+
+    analysis_mean = analysis_mean[nonzero_std_idx]
+    analysis_std = analysis_std[nonzero_std_idx]
+
+    normalize(track_audio_analysis_train, analysis_mean, analysis_std)
+    normalize(track_audio_analysis_valid, analysis_mean, analysis_std)
+    normalize(track_audio_analysis_test, analysis_mean, analysis_std)
 
     ## Create model
 
     track_name_layer = keras.Input(shape=track_names[0].shape)
     track_artists_layer = keras.Input(shape=track_artists[0].shape)
     track_album_name_layer = keras.Input(shape=track_album_names[0].shape)
-    track_audio_features_layer = keras.Input(shape=track_audio_features[0].shape)
-    track_audio_analysis_layer = keras.Input(shape=track_audio_analysis[0].shape)
+    track_audio_features_layer = keras.Input(shape=track_audio_features_train[0].shape)
+    track_audio_analysis_layer = keras.Input(shape=track_audio_analysis_train[0].shape)
 
     features = keras.layers.Concatenate()([
         track_name_layer,
